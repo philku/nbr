1ead4e734c93326e59eaeae0b1000cff787e70ba
philku <kubin@live.com>
2022-02-21 22:39:08 -0500

    0.0.1

---
+++ dist/index.d.ts
@@ -0,0 +1,19 @@
+/**
+ * nbr
+ *
+ * @param number
+ * @return {{local: string, local_dashed: string, domestic: string, domestic_dashed: string, domestic_dotted: string, domestic_areaParentheses: string, domestic_areaSpaced: string, domestic_areaSpacedParentheses: string, international: string, international_dashed: string}}
+ */
+export declare const nbr: (number: any) => {
+    local: any;
+    local_dashed: any;
+    domestic: any;
+    domestic_dashed: any;
+    domestic_dotted: any;
+    domestic_areaParentheses: any;
+    domestic_areaSpaced: any;
+    domestic_areaSpacedParentheses: any;
+    international: any;
+    international_dashed: any;
+};
+export default nbr;
